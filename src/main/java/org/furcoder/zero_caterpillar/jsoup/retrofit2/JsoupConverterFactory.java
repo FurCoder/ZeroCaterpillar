c36b774ee1d0801f85e1da92aa8a457a9be38c1f
mk124 <mk124a@gmail.com>
2019-03-23 02:35:19 +0800

    Improvement: replace anonymous class with lambda

--- src/main/java/org/furcoder/zero_caterpillar/jsoup/retrofit2/JsoupConverterFactory.java
+++ src/main/java/org/furcoder/zero_caterpillar/jsoup/retrofit2/JsoupConverterFactory.java
@@ -9,7 +9,6 @@ import retrofit2.Converter;
 import retrofit2.Retrofit;
 
 import javax.annotation.Nullable;
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -40,25 +39,21 @@ public class JsoupConverterFactory extends Converter.Factory
 
 		if (anno == null) return null;
 
-		return new Converter<>() {
-			@Nullable
-			@Override
-			public Object convert(ResponseBody body) throws IOException
-			{
-				Document document = Jsoup.parse(body.string());
-				Elements elements = document.select(anno.value());
+		return (body) ->
+		{
+			Document document = Jsoup.parse(body.string());
+			Elements elements = document.select(anno.value());
 
-				if (type == Elements.class) return elements;
-				if (type == String[].class) return elements.stream().map(e -> e.data()).toArray(String[]::new);
+			if (type == Elements.class) return elements;
+			if (type == String[].class) return elements.stream().map(e -> e.data()).toArray(String[]::new);
 
-				if (elements.isEmpty()) return null;
+			if (elements.isEmpty()) return null;
 
-				Element element = elements.get(0);
-				if (type == Element.class) return element;
-				if (type == String.class) return element.data();
+			Element element = elements.get(0);
+			if (type == Element.class) return element;
+			if (type == String.class) return element.data();
 
-				return null;
-			}
+			return null;
 		};
 	}
 }
